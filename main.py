5d51b0522f0d8f0e479d4fc4e0d7cc3a5e775b0f
Lhoyip98 <>
2021-04-13 14:53:00 +0000

    Update log --- V 0.0.3
    New functions:
    - Join calculation
    - Decimal degree convert to DDMMSS
    
    Improve user experience:
    - Redesign the menu

--- main.py
+++ main.py
@@ -1,30 +1,74 @@
 # My Surveying Calculater
 import math
-import cmath
+import cmath  # Calculation symbol
+import numpy  # Data analysis
+from tabulate import tabulate  # For create table
 
-
-modes = ('-> Distance calculation', '-> Join calculation')
+modes = ('-> Distance calculation: 1', '-> Join calculation: 2')
 
 # Set orgin values
 Dis = 0  # Distance value
 
+# Set a dictionary to store functons menu
+Function_menu = {
+    'Function': ['Distance Calculation', 'Join calculation', 'Exit'],
+    'Command': ['1', '2', '"e" or "E"']
+}
 
-# Function
+
+### Function
 # Distance function
 def Dis_cal(dN, dE):
     Dis = math.sqrt(dN * dN + dE * dE)
     return Dis
 
 
+# Join calculation ---> calculate the direction from A to B
+def Direct_cal(dN, dE):
+    WCB = math.atan(dE / dN)
+
+    if dN > 0 and dE > 0:
+        return float(WCB)
+    elif dN < 0 and dE > 0:
+        Adj_WCB = WCB + 180
+        return float(Adj_WCB)
+    elif dN < 0 and dE < 0:
+        return float(Adj_WCB)
+        Adj_WCB = WCB + 180
+    else:
+        Adj_WCB = WCB + 360
+        return float(Adj_WCB)
+
+
+# Decimal degree convert to DDMMSS
+def DDMMSS_convert(WCB):
+    DD = int(WCB)
+    # decimal_WCB = Adj_WCB - numpy.fix(Adj_WCB)     # get the Digits after the decimal point
+    decimal_WCB = WCB - DD
+    MM = decimal_WCB // 60
+    decimal_WCB = decimal_WCB % 60
+    SS = decimal_WCB
+    SS_2dp = float("%0.2f" % SS)
+
+    Final_WCB = print("The direction is: {}-{}-{} (DDMMSS)".format(
+        DD, MM, SS_2dp))
+    return Final_WCB
+
+
+### Main programme code
+
 # Introduction
 print("Welcome to my Calculater")
 print()
 print()
 # Show which funtions are support
-print("Support function modes")
-for items in modes:
-  print(items)
+print("Support function modes\n")
+print(tabulate(Function_menu, headers='keys'))
 print()
+# Useless for loop the menu
+#for items in modes:
+#  print(items)
+#print()
 
 # Choose function.
 while True:
@@ -45,38 +89,21 @@ while True:
         print()
         print()
 
-    # Stope the calculater
+    elif mode == '2':
+        # Join calculation
+        print("Join calculation mode \n")
+        # input ΔN and ΔE
+        delta_N = float(input("Please input the ΔN: "))
+        delta_E = float(input("Please input the ΔE: "))
+        print()
+        # Result
+        result = Direct_cal(delta_N, delta_E)
+        print("The bearing between A to B: {}".format(float(result)))
+
+    # Stop the calculater
     elif mode == 'e' or 'E':
         break
         print("See you ~")
 
     else:
         print("Error !! Please try again.")
-
-
-
-# Notes for amount
-# int() is interger
-# float() can input decimals
-# math.floor(n) will return the closest integer value
-# math.trunc(n) cut d.p. just keep integer number
-# '//' Rounds down to nearest integer
-# math.sqrt() to find the square root of any positive real number (integer or decimal)
-# isqrt() returns the integer square root of a non-negative integer.
-
-# Degree and radians Translate:
-# math.radians() convert degrees to radians
-# math.degrees() convert radians to degrees
-
-# Trigonometric Values:
-# math.sin(), math.cos(), math.tan()
-# math.asin(), math.acos(), math.atan()
-# math.hypot() hypotenuse of a triangle
-
-
-# Problem need to be fixed
-  # 存儲變量
-    # 每次重啓？
-    # 儲存變量 N,E
-    # 會否在使用下一個function時，產生混亂
-      # need detact
